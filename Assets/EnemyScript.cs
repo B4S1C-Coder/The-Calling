e067f03b61ef56969f63beef8b4c9cbc97f24af4
B4S1C-Coder <saksham2005mittal@gmail.com>
2024-11-11 17:29:13 +0000

    Added Creep Animation Controller. Attempted enemy animation trigger. (Failed)

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,17 +1,49 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
     public float entityHealth = 100f;
-    
+    public Animator enemyAnimator;
+    private bool isDying = false;
+    void Start() {
+        // enemyAnimator = GetComponent<Animator>();
+
+        if (enemyAnimator == null) {
+            Debug.LogWarning("enemyAnimator is NULL.");
+        }
+    }
+
     void Update()
     {
-        if (entityHealth <= 0) {
-            Destroy(gameObject);
+        if (entityHealth <= 0 && !isDying) {
+            // enemyAnimator.SetBool("isDead", true);
+            // Destroy(gameObject);
+
+            StartCoroutine(PlayDeathAnimation());
         }
     }
 
     public void TakeDamage(float damageAmount) {
         entityHealth -= damageAmount;
+
+        if (enemyAnimator != null) {
+            enemyAnimator.Rebind();
+            enemyAnimator.SetTrigger("Damage");
+            Debug.Log("Trigger set.");
+        }
+    }
+
+    private IEnumerator PlayDeathAnimation() {
+        isDying = true;
+        enemyAnimator.SetTrigger("Death");
+
+        while (enemyAnimator.IsInTransition(0) || enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Creep|Death_Action")) {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(gameObject);
     }
 }
